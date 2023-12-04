db2158d7c40763b89bba6693ab54ce652d32464c
Marcos Renê <renemoura789@gmail.com>
2023-12-04 17:46:00 -0300

    feat: add ConnectWallet ui component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,21 @@
-import '../styles/globals.css'
-import Progress from 'nextjs-progressbar';
+import "../styles/globals.css";
+import { ThirdwebProvider } from "@thirdweb-dev/react";
+import Progress from "nextjs-progressbar";
 
-import Layout from './_layout'
+import Layout from "./_layout";
 
 export default function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-      <Progress
-        color="#FF2748"
-        startPosition={0.3}
-        stopDelayMs={200}
-        height={3}
-      />
-    </Layout>
-  )
+    <ThirdwebProvider activeChain="sepolia">
+      <Layout>
+        <Component {...pageProps} />
+        <Progress
+          color="#FF2748"
+          startPosition={0.3}
+          stopDelayMs={200}
+          height={3}
+        />
+      </Layout>
+    </ThirdwebProvider>
+  );
 }
-
--- pages/_navbar.tsx
+++ pages/_navbar.tsx
@@ -1,20 +1,25 @@
-import { Logo } from "assets/Logo";
 import { useRouter } from "next/router";
+import { ConnectWallet } from "@thirdweb-dev/react";
+
+import { Logo } from "assets/Logo";
 
 export default function Navbar() {
-  const router = useRouter()
+  const router = useRouter();
 
   return (
-    <header>
-      <div 
-        onClick={() => router.push('/')}
-        className="h-2 px-24 py-10 flex items-center cursor-pointer" 
-      >
-        <Logo />
-        <p className="ml-3 font-semibold">Coders NFT</p>
-      </div>
+    <>
+      <header className="h-20 px-[10%] py-10 flex items-center justify-between">
+        <div
+          onClick={() => router.push("/")}
+          className="flex items-center cursor-pointer"
+        >
+          <Logo />
+          <p className="ml-3 font-semibold">Coders NFT</p>
+        </div>
+
+        <ConnectWallet modalSize="compact" />
+      </header>
       <hr className="w-full border-[#242634]" />
-    </header>
+    </>
   );
 }
-
